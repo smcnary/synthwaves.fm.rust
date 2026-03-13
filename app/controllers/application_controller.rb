01e91ae5787d95ecc5cb2de2efc253eb1033445f
leopolicastro <43428385+leopolicastro@users.noreply.github.com>
2026-03-13 14:50:54 -0400

    allow older browsers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   include Orderable
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
